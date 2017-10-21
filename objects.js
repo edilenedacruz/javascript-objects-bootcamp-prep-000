67f909a5489c50287847637d14b42110a9cc19a1
Edilene Da Cruz <hello@edilene.io>
2017-10-21 23:08:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,7 @@ var playlist = {
   "Aerosmith": "Walk this way",
   "Queens": "We will rock you"
 }
+
+function updatePlaylist(object, key, value){
+  return Object.assign({}, object, {[key]: value})
+}
